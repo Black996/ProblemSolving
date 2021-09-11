da60637e21a74968dd40d895954d6906f39580ef
black996 <bl4ck.96@yandex.com>
2021-09-11 18:15:26 +0300

    added kyu6 bouncingBall and kyu5 rgb

--- Codewars/kyu5.js
+++ Codewars/kyu5.js
@@ -42,3 +42,39 @@ var moveZeros = function (arr) {
   }
   return arr;
 };
+
+// ## Problem: RGB To Hex Conversion ##
+// The rgb function is incomplete. Complete it so that passing in RGB decimal values will result in
+// a hexadecimal representation being returned. Valid decimal values for RGB are 0 - 255.
+// Any values that fall out of that range must be rounded to the closest valid value.
+// Note: Your answer should always be 6 characters long, the shorthand with 3 will not work here.
+
+// ## Solution ##
+
+function rgb(r, g, b) {
+  // complete this function
+  r = r < 0 ? 0 : r > 255 ? 255 : r;
+  g = g < 0 ? 0 : g > 255 ? 255 : g;
+  b = b < 0 ? 0 : b > 255 ? 255 : b;
+
+  r =
+    r.toString(16) == 0
+      ? "00"
+      : r.toString(16).toUpperCase().length == 1
+      ? `0${r.toString(16).toUpperCase()}`
+      : r.toString(16).toUpperCase();
+  g =
+    g.toString(16) == 0
+      ? "00"
+      : g.toString(16).toUpperCase().length == 1
+      ? `0${g.toString(16).toUpperCase()}`
+      : g.toString(16).toUpperCase();
+  b =
+    b.toString(16) == 0
+      ? "00"
+      : b.toString(16).toUpperCase().length == 1
+      ? `0${b.toString(16).toUpperCase()}`
+      : b.toString(16).toUpperCase();
+
+  return `${r}${g}${b}`;
+}
